5106cb6cef54d50bdcf89e30d0fa0dfb5d6ed217
Judzhin Miles <info@msbios.com>
2017-09-10 23:44:48 +0300

    Lazy Composer Commiter

--- config/module.config.php
+++ config/module.config.php
@@ -10,41 +10,7 @@ use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
 
-    'doctrine' => [
-        'connection' => [
-            'orm_default' => [
-                'driverClass' => \Doctrine\DBAL\Driver\PDOMySql\Driver::class,
-                'params' => [
-                    'host' => '',
-                    'user' => '',
-                    'password' => '',
-                    'dbname' => '',
-                ],
-            ],
-        ],
-        'driver' => [
-            // defines an annotation driver with two paths, and names it `my_annotation_driver`
-            Module::class => [
-                'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
-                'cache' => 'array',
-                'paths' => [
-                    __DIR__ . '/../src/Entity'
-                ],
-            ],
-
-            // default metadata driver, aggregates all other drivers into a single one.
-            // Override `orm_default` only if you know what you're doing
-            'orm_default' => [
-                'drivers' => [
-                    Entity::class => Module::class
-                ]
-            ]
-        ]
-    ],
     'hydrators' => [
-        'factories' => [
-
-        ],
         'aliases' => [
             \MSBios\Resource\Form\LayoutForm::class =>
                 \DoctrineModule\Stdlib\Hydrator\DoctrineObject::class,
